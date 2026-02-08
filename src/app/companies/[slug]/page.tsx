e66ced8658ec0bddaed85dee24a30299d593ad6b
SoumyadipRoy16 <roysoumyadip334@gmail.com>
2026-02-08 19:17:40 +0530

    issue resolve

--- src/app/companies/[slug]/page.tsx
+++ src/app/companies/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { notFound } from 'next/navigation'
 import Navbar from '../../components/Navbar'
 import { API_BASE_URL } from '../../../lib/config'
 
+export const dynamicParams = false
+
 type PageProps = {
   params: Promise<{ slug: string }>
 }
